1a6d604010dd7de703236718d9d66ef2fbeb6efd
Anna Mucea <annamucea@dyn172-23-35-118.wireless.uwo.pri>
2022-11-19 02:39:33 -0500

    menu styling

--- hack-western-app/src/App.js
+++ hack-western-app/src/App.js
@@ -1,18 +1,21 @@
 import Dashboard from "./Dashboard.jsx"
 import Menu from "./Menu.jsx"
+import './Menu.css';
 import './App.css';
+import './TextBoxes.css';
 import React from "react";
 
 function App() {
   const [isHome, updateIsHome] = React.useState(false);
   return (
     <div>
-      <header>
+      <div className="menu-bar">
+        <Menu isHome={isHome} updateIsHome={updateIsHome}/>
+        {isHome ? <Dashboard isHome={isHome} updateIsHome={updateIsHome}/> : ""}
+      </div>
+      <header className="text-box">
         Hello Blue! 
       </header>
-      <Menu isHome={isHome} updateIsHome={updateIsHome}/>
-      
-      {isHome ? <Dashboard isHome={isHome} updateIsHome={updateIsHome}/> : "my info"}
     </div>
   );
 }
