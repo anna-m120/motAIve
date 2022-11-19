b6e3fba3ef7036358c6dc03d4b4c3af53c56320d
Anna Mucea <annamucea@dyn172-23-35-118.wireless.uwo.pri>
2022-11-19 06:45:58 -0500

    menu buttons plus menu sticky

--- hack-western-app/src/Menu.jsx
+++ hack-western-app/src/Menu.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import './Menu.css';
 function Menu({updateIsHome}) {
   return (
     <div>
-      <button onClick={() =>  updateIsHome(true)}>Home</button>
-      <button onClick={() =>  updateIsHome(false)}>My Info</button>
+      <button className="button" onClick={() =>  updateIsHome(true)}>Home</button>
+      <button className="button" onClick={() =>  updateIsHome(false)}>My Info</button>
+      <button className="log-out-button" onClick={() =>  updateIsHome(false)}>Log Out </button>
     </div>
   );
 }
